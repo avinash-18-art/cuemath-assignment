9af8409a4ab0d164da94c1fff1bc8cfd4dd0a25b
avinash <chauhanavinash36869@gmail.com>
2024-03-27 06:56:42 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import React, {useState} from 'react'
 import './App.css'
 
-const App = () => <div>Hello World</div>
+function App() {
+  const [rookPosition, setRookPosition] = useState(63)
+
+  const handleSquareClick = index => {
+    const rookRow = Math.floor(rookPosition / 8)
+    const clickedRow = Math.floor(index / 8)
+    const rookCol = rookPosition % 8
+    const clickedCol = index % 8
+
+    if (index === rookPosition) return
+
+    // Move down
+    if (clickedCol === rookCol && clickedRow > rookRow) {
+      setRookPosition(index)
+    }
+    // Move left
+    else if (clickedRow === rookRow && clickedCol < rookCol) {
+      setRookPosition(index)
+    }
+  }
+
+  const renderChessboard = () => {
+    const squares = []
+    for (let i = 0; i < 64; i++) {
+      const isLightSquare = (Math.floor(i / 8) + i) % 2 === 0
+      const isRookSquare = i === rookPosition
+      squares.push(
+        <div
+          key={i}
+          className={`chess-square ${isLightSquare ? 'light' : 'dark'} ${
+            isRookSquare ? 'rook' : ''
+          }`}
+          onClick={() => handleSquareClick(i)}
+        />,
+      )
+    }
+    return squares
+  }
+
+  return <div id="chessboard">{renderChessboard()}</div>
+}
 
 export default App
